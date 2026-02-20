9331f7b73c4ffce8c76e9987b8526eb0d25b7d33
João <joaonodari2017@hotmail.com>
2026-02-20 11:54:10 -0300

    fix(db): atualizar estrutura do banco de dados

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,25 @@
+import os
 import psycopg2
+from urllib.parse import urlparse
+from dotenv import load_dotenv # Importa a biblioteca
+
+# Carrega as variáveis do arquivo .env para o sistema
+load_dotenv()
 
 def get_connection():
+    # Agora o os.getenv vai buscar a URL de forma segura
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise ValueError("ERRO: Variável DATABASE_URL não encontrada no arquivo .env")
+
+    result = urlparse(database_url)
+
     return psycopg2.connect(
-        host="172.16.0.139",
-        database="Teste",
-        user="joao",
-        password="Peol@2016",
-        port="5432"
+        host=result.hostname,
+        database=result.path[1:],
+        user=result.username,
+        password=result.password,
+        port=result.port,
+        sslmode='require'
     )
