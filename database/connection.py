309f428adcb41b21136acf80e1971bd6328e9b34
João <joaonodari2017@hotmail.com>
2026-08-28 16:46:23 -0300

    chore: configura ambiente local de desenvolvimento

--- database/connection.py
+++ database/connection.py
@@ -1,25 +1,14 @@
 import os
-import psycopg2
-from urllib.parse import urlparse
-from dotenv import load_dotenv # Importa a biblioteca
 
-# Carrega as variáveis do arquivo .env para o sistema
-load_dotenv()
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv() # Carrega as variáveis do arquivo .env para o sistema
 
 def get_connection():
-    # Agora o os.getenv vai buscar a URL de forma segura
     database_url = os.getenv("DATABASE_URL")
 
     if not database_url:
-        raise ValueError("ERRO: Variável DATABASE_URL não encontrada no arquivo .env")
+        raise RuntimeError("DATABASE_URL não configurada.")
 
-    result = urlparse(database_url)
-
-    return psycopg2.connect(
-        host=result.hostname,
-        database=result.path[1:],
-        user=result.username,
-        password=result.password,
-        port=result.port,
-        sslmode='require'
-    )
+    return psycopg2.connect(database_url)
